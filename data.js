0d7c392b0618bb026b5b9edc10b338be26155f10
tanjil2040 <62055525+tanjil2040@users.noreply.github.com>
2021-12-06 20:37:57 +0600

    Update data.js

--- data.js
+++ data.js
@@ -1,45 +1,19 @@
 module.exports = function () {
     return {
         products: [
-            {
-                id: 1, name: "Kayak", category: "Watersports",
-                description: "A boat for one person", price: 275
-            },
-            {
-                id: 2, name: "Lifejacket", category: "Watersports",
-                description: "Protective and fashionable", price: 48.95
-            },
-            {
-                id: 3, name: "Soccer Ball", category: "Soccer",
-                description: "FIFA-approved size and weight", price: 19.50
-            },
-            {
-                id: 4, name: "Corner Flags", category: "Soccer",
-                description: "Give your playing field a professional touch",
-                price: 34.95
-            },
-            {
-                id: 5, name: "Stadium", category: "Soccer",
-                description: "Flat-packed 35,000-seat stadium", price: 79500
-            },
-            {
-                id: 6, name: "Thinking Cap", category: "Chess",
-                description: "Improve brain efficiency by 75%", price: 16
-            },
-            {
-                id: 7, name: "Unsteady Chair", category: "Chess",
-                description: "Secretly give your opponent a disadvantage",
-                price: 29.95
-            },
-            {
-                id: 8, name: "Human Chess Board", category: "Chess",
-                description: "A fun game for the family", price: 75
-            },
-            {
-                id: 9, name: "Bling Bling King", category: "Chess",
-                description: "Gold-plated, diamond-studded King", price: 1200
-            }
-        ],
+            { id: 1, name: "Speed Boat", category: "Watersports", description: "A boat for two persons", price: 275, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvV43ehNDMjnYZSyH9CmLRUO9zOiHgmiww6Q&usqp=CAU'},
+            { id: 2, name: "Lifejacket", category: "Watersports", description: "Protective and fashionable", price: 48.50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRblJ6yLtgDkA96cGkErG8754SO8gjjbGCm1Q&usqp=CAU'},
+            { id: 3, name: "Soccer Ball", category: "Soccer", description: "FIFA-approved size and weight", price: 39.50,image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMFAyIGcPBhXjgfG0IyGp9nmkCNU_2IpTcEw&usqp=CAU'},
+            { id: 4, name: "Corner Flags", category: "Soccer", description: "Give your playing field a professional touch", price: 14.50,image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtG71cD1tcX4-v7mN1JZamZrA9oGA-FrsBjQ&usqp=CAU'},
+            { id: 5, name: "Soccer Boot", category: "Soccer", description: "Various kinds of Boot", price: 79.50,image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzckCST3HZ5jXaOcTOwEkzG9JLZVK4ZIS2Lw&usqp=CAU'},
+            { id: 6, name: "Thinking Cap", category: "Chess", description: "Improve brain efficiency by 75%", price: 16,image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8q_jjZQEaTvye2Y9fhyV7caYnq0QdXS4m7g&usqp=CAU'},
+            { id: 7, name: "Unsteady Chair", category: "Chess", description: "Secretly give your opponent a disadvantege", price: 29.95,image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwjHoeYm1ErhMZpk7EQzHAiyMDS7Pi9HXGEw&usqp=CAU'},
+            { id: 8, name: "Chess Board", category: "Chess", description: "A fun game for the family", price: 25,image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS010dySPZI4lFBLPfvjJ1eDLUDPEz_spHxww&usqp=CAU'},
+            { id: 9, name: "Cricket Bat", category: "Cricket", description: "Helpful for hard hitting", price: 175,image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShHyDZQ7EeryCcmQJ05hPaV7Ast8lqc_bPMg&usqp=CAU'},
+            { id: 10, name: "Cricket Ball", category: "Cricket", description: "Helpful for swinging", price: 35,image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_hKt23Us-Auq-fybDwQTjm9Ba0Xa1A5Lixg&usqp=CAU'},
+            { id: 11, name: "Cricket Stump", category: "Cricket", description: "It has lightening sensor", price: 250,image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6_iGkUQ6xJt081-LvOumBdXH4d1FOo_xT1w&usqp=CAU'},
+            { id: 12, name: "Cricket Gloves", category: "Cricket", description: "Very soft and lightweight", price: 75,image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9qIQz-XD8v6F1gf-6Gg_q2GyOKRt9KBht_w&usqp=CAU'}
+             ],
         orders: []
     }
 }
